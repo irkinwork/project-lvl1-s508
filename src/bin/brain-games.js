d5567bfe2460042bd170d5575bf71145ce819a39
Irina Nikolaidi <irin.work42@gmail.com>
2019-05-21 03:00:27 +0300

    finish 3 step

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import {name} from '..';
-console.log("Welcome to the Brain Games!");
+import name from '..';
+
+console.log('Welcome to the Brain Games!');
 console.log(`Hello, ${name}!`);
