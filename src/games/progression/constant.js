41dc6fbe22ec4d9a39c0a06a5edad93ab60566ed
Irina Nikolaidi <irin.work42@gmail.com>
2019-05-26 15:48:08 +0300

    fix 7step

--- src/games/progression/constant.js
+++ src/games/progression/constant.js
@@ -13,10 +13,14 @@ export const data = () => {
     }
     return getTenItemsOFprogression(nn + 1, arr.concat(formula(nn)));
   };
+  const parseProgToString = (prog) => {
+    const arr = prog;
+    arr.splice(order, 1, '..');
+    return arr.join(' ');
+  };
   const resultProgression = getTenItemsOFprogression();
   const correctAnswer = String(resultProgression[order]);
-  const resultStr = resultProgression;
-  resultStr.splice(order, 1, '..');
-  const question = `${resultStr.join(' ')}`;
+  const resultStr = parseProgToString(resultProgression);
+  const question = `${resultStr}`;
   return { correctAnswer, question };
 };
