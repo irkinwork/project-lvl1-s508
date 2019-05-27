584c9b537298a7cae0b284a4ab9a18e045aea604
Irina Nikolaidi <irin.work42@gmail.com>
2019-05-28 01:43:51 +0300

    change const names

--- src/games/progression/index.js
+++ src/games/progression/index.js
@@ -7,21 +7,21 @@ const progressionLength = 10;
 export const data = () => {
   const first = getRandomNumber();
   const difference = getRandomNumber();
-  const order = Math.abs(getRandomNumber(progressionLength));
-  const formula = n => first + (n - 1) * difference;
-  const getTenItemsOFprogression = (nn = 0, arr = []) => {
-    if (nn === progressionLength) {
-      return arr;
+  const randomIndex = Math.abs(getRandomNumber(progressionLength));
+  const getProgressionItem = index => first + (index - 1) * difference;
+  const buildProgression = (length, index = 0, array = []) => {
+    if (index === length) {
+      return array;
     }
-    return getTenItemsOFprogression(nn + 1, arr.concat(formula(nn)));
+    return buildProgression(length, index + 1, array.concat(getProgressionItem(index)));
   };
-  const parseProgToString = (prog) => {
-    const arr = prog;
-    arr.splice(order, 1, '..');
+  const parseProgToString = (progression) => {
+    const arr = progression;
+    arr.splice(randomIndex, 1, '..');
     return arr.join(' ');
   };
-  const resultProgression = getTenItemsOFprogression();
-  const correctAnswer = String(resultProgression[order]);
+  const resultProgression = buildProgression(progressionLength);
+  const correctAnswer = String(resultProgression[randomIndex]);
   const question = parseProgToString(resultProgression);
   return { correctAnswer, question };
 };
